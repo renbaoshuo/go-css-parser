302386068e720dcdcaf58456cdb716784870be36
Baoshuo <i@baoshuo.ren>
2025-09-23 15:18:39 +0800

    token_stream: allow to commit changes even if block consumer failed

--- token_stream/block.go
+++ token_stream/block.go
@@ -7,6 +7,9 @@ import (
 	"go.baoshuo.dev/csslexer"
 )
 
+// SetBoundary sets or unsets a token type as a boundary token.
+// When the token stream encounters a boundary token, it may stop
+// processing further tokens depending on the context.
 func (ts *TokenStream) SetBoundary(tokenType csslexer.TokenType, enable bool) {
 	if enable {
 		ts.b[tokenType] = true
@@ -15,19 +18,32 @@ func (ts *TokenStream) SetBoundary(tokenType csslexer.TokenType, enable bool) {
 	}
 }
 
+// ConsumeBlockToEndWithRestoring consumes a block of tokens until the end token is reached,
+// allowing the caller to process the tokens within the block using the provided blockConsumer
+// function.
+//
+// If the blockConsumer function returns commit as false, the token stream state is restored
+// to its initial state before the block was consumed. Note this behavior is not affected by
+// whether an error is returned or not.
+//
+// The errors returned by blockConsumer are wrapped with additional context.
 func (ts *TokenStream) ConsumeBlockToEndWithRestoring(
 	endTokenType csslexer.TokenType,
-	blockConsumer func(ts *TokenStream) (commit bool, err error)) error {
+	blockConsumer func(ts *TokenStream) (commit bool, err error),
+) error {
 	initialState := ts.State()
 	ob := maps.Clone(ts.b) // Clone the current boundaries to restore later if needed
 	ts.b = make(map[csslexer.TokenType]bool)
+	defer func() { ts.b = ob }() // Always restore boundaries
 
 	ts.Consume()                       // Consume the block start token
 	ts.SetBoundary(endTokenType, true) // Set the boundary for the end token
 
 	commit, err := blockConsumer(ts)
 	if err != nil {
-		initialState.Restore() // Restore the initial state in case of error
+		if !commit {
+			initialState.Restore() // Restore the initial state if not committing
+		}
 		return fmt.Errorf("error while consuming block: %w", err)
 	}
 
@@ -44,12 +60,18 @@ func (ts *TokenStream) ConsumeBlockToEndWithRestoring(
 	}
 
 	ts.Consume() // Consume the end token
-	ts.b = ob    // Restore the boundaries
 	return nil
 }
 
-func (ts *TokenStream) ConsumeBlockRestoring(
-	blockConsumer func(ts *TokenStream) (commit bool, err error)) error {
+// ConsumeBlockRestoring consumes a block of tokens, allowing the caller to process
+// the tokens within the block using the provided blockConsumer function.
+//
+// If the blockConsumer function returns commit as false, the token stream state is restored
+// to its initial state before the block was consumed. Note this behavior is not affected by
+// whether an error is returned or not.
+//
+// The errors returned by blockConsumer are wrapped with additional context.
+func (ts *TokenStream) ConsumeBlockRestoring(blockConsumer func(ts *TokenStream) (commit bool, err error)) error {
 	startToken := ts.Peek()
 
 	if !isBlockStartToken(startToken.Type) {
@@ -61,24 +83,29 @@ func (ts *TokenStream) ConsumeBlockRestoring(
 	return ts.ConsumeBlockToEndWithRestoring(endTokenType, blockConsumer)
 }
 
+// ConsumeBlock consumes a block of tokens, allowing the caller to process
+// the tokens within the block using the provided blockConsumer function.
+//
+// If an error occurs during processing, the token stream will skip to the end of the block
+// and the error will be returned with additional context.
 func (ts *TokenStream) ConsumeBlock(blockConsumer func(ts *TokenStream) error) error {
 	return ts.ConsumeBlockRestoring(func(ts *TokenStream) (commit bool, err error) {
-		err = blockConsumer(ts)
-		if err != nil {
-			return false, err // If an error occurs, do not commit the block
-		}
-		return true, nil // Commit the block if no error
+		return true, blockConsumer(ts)
 	})
 }
 
+// ConsumeBlockToEnd consumes a block of tokens until the specified end token
+// type is reached, allowing the caller to process the tokens within the block
+// using the provided blockConsumer function.
+//
+// If an error occurs during processing, the token stream state is restored to
+// its initial state before the block was consumed, and the error is returned
+// with additional context.
 func (ts *TokenStream) ConsumeBlockToEnd(
 	endTokenType csslexer.TokenType,
-	blockConsumer func(ts *TokenStream) error) error {
+	blockConsumer func(ts *TokenStream) error,
+) error {
 	return ts.ConsumeBlockToEndWithRestoring(endTokenType, func(ts *TokenStream) (commit bool, err error) {
-		err = blockConsumer(ts)
-		if err != nil {
-			return false, err // If an error occurs, do not commit the block
-		}
-		return true, nil // Commit the block if no error
+		return true, blockConsumer(ts)
 	})
 }
